fdd6d3a5dc18c2e7273c2b5033ac3a50cca1e800
Anuj Thakur <anujthakur462000@gmail.com>
2023-11-07 18:25:41 +0530

    some edits to the code

--- 04bg-changer/src/App.jsx
+++ 04bg-changer/src/App.jsx
@@ -8,7 +8,54 @@ function App() {
     <>
       <div className="w-full h-screen duration-200"
       style= {{ backgroundColor: color}}
-      ></div>
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2"> 
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "red"}}
+            >Red</button>
+
+            <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "blue"}}
+            >blue</button>
+
+            <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "green"}}
+            >green</button>
+
+            <button
+            onClick={() => setColor("black")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "black"}}
+            >black</button>
+
+            <button
+            onClick={() => setColor("pink")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "pink"}}
+            >pink</button>
+
+            <button
+            onClick={() => setColor("violet")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "violet"}}
+            >violet</button>
+
+            <button
+            onClick={() => setColor("orange")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "orange"}}
+            >orange</button>
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
