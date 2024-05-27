b23e227b34f5bf17e8f6bd1914b2ddb3b42840f2
Anuj <anuj.thakur@formulaq.io>
2024-05-27 22:54:47 +0530

    generator

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useCallback } from 'react'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = 
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(numAllowed) str += "0123456789"
+    if(numAllowed) str += "!@#$%^&*-+=[]{}~`"
+
+  }, length, numAllowed, charAllowed, setPassword)
+
+  return (
+    <>
+      <h1 className=" text-4xl text-center text-white" >Password generator</h1>
+    </>
+  )
+}
+
+export default App
